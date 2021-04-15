3e1400c415160cd7bf9148a84afd0c4f6f6d30d0
alejo <tqaw19@gmail.com>
2021-04-14 23:33:32 -0500

    some changes

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,24 +1,25 @@
-require('dotenv').config();
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./schema')
-const { createStore } = require('./utils')
+require("dotenv").config();
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const { createStore } = require("./utils");
 
-const LaunchAPI = require('./datasources/launch');
-const UserAPI = require('./datasources/user');
+const LaunchAPI = require("./datasources/launch");
+const UserAPI = require("./datasources/user");
 
-const store = createStore()
+const store = createStore();
 
 const server = new ApolloServer({
-    typeDefs,
-    dataSources: () => ({
-        launchAPI: new LaunchAPI(),
-        userAPI: new UserAPI({ store })
-    })
-})
+  typeDefs,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store }),
+  }),
+});
 
 server.listen().then(() => {
-    console.log(`
+  console.log(`
         Server is running!
         Listening on port 4000
-    `)
-})
+    `);
+});
+
